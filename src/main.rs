cf3e78202450ffcb4ca9b1ed37c9f6d1a8d0950b
Eric Smith <paytonrules@gmail.com>
2024-05-28 11:51:23 -0500

    Generate an enemy in a random location

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use bevy::prelude::*;
+use rand::{random, thread_rng, Rng};
 
 mod player;
 
 const PLAYER_INDEX: usize = 84;
 const ENEMY_INDICES: [usize; 14] = [85, 86, 87, 88, 96, 97, 98, 99, 100, 108, 109, 110, 111, 112];
+const MIN_DISTANCE_TO_ENEMY: f32 = 140.0;
+const MAX_DISTANCE_TO_ENEMY: f32 = 550.0;
 
 #[derive(Component)]
 struct EnemyTag;
@@ -40,18 +43,19 @@ fn setup(
 
     let texture_atlas_layout = texture_atlas_layouts.add(layout);
 
+    let player_transform = Transform::from_scale(Vec3::splat(3.0));
     let sprite_template = SpriteSheetBundle {
         texture: texture.clone(),
         atlas: TextureAtlas {
             layout: texture_atlas_layout.clone(),
             index: 0,
         },
-        transform: Transform::from_scale(Vec3::splat(3.0)),
+        transform: player_transform,
         ..default()
     };
 
     spawn_player(sprite_template.clone(), &mut commands);
-    spawn_enemies(sprite_template.clone(), &mut commands);
+    spawn_enemies(sprite_template.clone(), &player_transform, &mut commands);
 }
 
 fn spawn_player(mut bundle: SpriteSheetBundle, commands: &mut Commands) {
@@ -59,9 +63,33 @@ fn spawn_player(mut bundle: SpriteSheetBundle, commands: &mut Commands) {
     commands.spawn((player::PlayerTag, bundle));
 }
 
-fn spawn_enemies(mut bundle: SpriteSheetBundle, commands: &mut Commands) {
+fn spawn_enemies(
+    mut bundle: SpriteSheetBundle,
+    player_transform: &Transform,
+    commands: &mut Commands,
+) {
+    let mut rng = thread_rng();
+    let length = Vec3 {
+        x: rng.gen_range(MIN_DISTANCE_TO_ENEMY..MAX_DISTANCE_TO_ENEMY),
+        y: 0.0,
+        z: 0.0,
+    };
+    let mut direction = None;
+    while direction.is_none() {
+        direction = Vec3 {
+            x: rng.gen_range(-1.0..1.0),
+            y: rng.gen_range(-1.0..1.0),
+            z: 0.0,
+        }
+        .try_normalize();
+    }
+    let direction =
+        direction.unwrap() * rng.gen_range(MIN_DISTANCE_TO_ENEMY..MAX_DISTANCE_TO_ENEMY);
+
     bundle.atlas.index = 85;
-    bundle.transform = Transform::from_scale(Vec3::splat(2.0));
+    bundle.transform = player_transform
+        .with_scale(Vec3::splat(2.0))
+        .with_translation(direction);
 
     commands.spawn((EnemyTag, bundle));
 }
